bffb7fca5e2994036b8eee08aa13734e52a52bf3
adamwysocki <adam@adamwysocki.me>
2018-02-28 22:28:30 -0500

    clean up some comments

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import chalk from "chalk";
 import cheerio from "cheerio";
 import pkg from "../package";
 
-// setup some conts
+// setup some consts
 const PORT = process.env.PORT || 8080;
 const WEB_PAGE = `https://www.captainu.com`;
 
@@ -18,13 +18,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 
-// middleware for all routes, makes an async request
-// to get a webpage and waits for the result to set
-// on the request. This could be a db call, or other
-// async API as well
+/* 
+    middleware for all routes, makes an async request
+    to get a webpage and waits for the result to set
+    on the request. This could be a db call, or other
+    async API as well 
+*/
 app.use(async (request, response, next) => {
   const result = await axios.get(WEB_PAGE);
   const $ = cheerio.load(result.data);
+
+  /* 
+    pageTitle will be available to all routes
+    via the request obj. This could be results from
+    a login or something session specific. 
+  */
+
+  /* 
+    ideally this would be cached in a session instead of lookup
+    on every route call. 
+  */
   request.pageTitle = $("title").text();
   next();
 });
@@ -34,6 +47,7 @@ app.get("/", (request, response) => {
   response.status(200).json({ result: "success", code: 200, data: { title: request.pageTitle } });
 });
 
+// fire up the server
 app.server = app.listen(PORT, () =>
   console.log(chalk.blue(`${pkg.name} version ${pkg.version} running on port ${PORT}`))
 );
